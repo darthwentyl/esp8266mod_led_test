cc12fc3118fa075d0ff967e48eafc6a59e7240ea
darthwentyl <michal.wendland@gmail.com>
2018-11-05 23:02:21 +0100

    Arduino: Simple tcp server implementation

--- esp8266mod_src/tcp_ip_wifi_led/tcp_ip_wifi_led/tcp_ip_wifi_led.ino
+++ esp8266mod_src/tcp_ip_wifi_led/tcp_ip_wifi_led/tcp_ip_wifi_led.ino
@@ -2,13 +2,48 @@
 #include <WiFiClient.h>
 #include <WiFiServer.h>
 
-WiFiServer server(8888);
+#define LED 12
 
-void setup(){
+const char *ssid = "darth_wentyl";
+const char *password = "1234567890";
+
+WiFiServer server(8888);
+WiFiClient client;
+
+bool isLedOn = false;
+
+void setup() {
+  Serial.begin(115200);
   
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);  
+
+  Serial.println();
+  Serial.print("Server IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  pinMode(LED, OUTPUT); 
+  digitalWrite(LED, HIGH);
+
+  server.begin();
+  Serial.println("Wait for tcp client connection");
 }
 
-void loop(){
-  
+void loop() {
+  if (!client.connected()) {
+    client = server.available();
+    delay(100);
+    if (isLedOn) {
+      digitalWrite(LED, HIGH);
+      isLedOn = !isLedOn;
+    } else {
+      digitalWrite(LED, LOW);
+      isLedOn = !isLedOn;
+    }
+  } else {
+    if (client.available() > 0) {
+      Serial.print((char)client.read());
+    }
+  }
 }
 
